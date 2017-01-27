fefbfc05f65ce60dc17f28766848e442677b30f3
Andreas Wassmer <menkalinan56@gmail.com>
2017-01-27 16:51:42 +0100

    Added class for humidity sensor.

---
+++ src/simdevice/HumiditySensor.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simdevice;
+
+/**
+ *
+ * @author andreaswassmer
+ */
+public class HumiditySensor extends Sensor implements Measuring, SensorType {
+
+    public HumiditySensor() {
+        super();
+        this.setRange(0.0, 100.0);
+        name = "Humidity";
+        type = SensorType.HUMIDITY;
+    }
+    
+    @Override
+    public String measure() {
+        double res = this.readValueAsDouble();
+        return String.valueOf(res);
+    }
+}
