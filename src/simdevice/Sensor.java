1d3dde91eed720eba28c1d4735e8b2d296292db5
Andreas Wassmer <menkalinan56@gmail.com>
2017-01-27 15:41:50 +0100

    Added abstract class Sensor.

---
+++ src/simdevice/Sensor.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simdevice;
+
+import java.util.Random;
+
+/**
+ * Implements a simple virtual sensor
+ * @author andreaswassmer
+ */
+public abstract class Sensor {
+    String name;
+    int type;
+    Double rangeStart, rangeEnd, range;
+    Random reading = new Random();
+    
+    public void setRange(int start, int end) {
+        if (start > end) {
+            rangeStart = new Double(end);
+            rangeEnd = new Double(start);
+        } else {
+            rangeStart = new Double(start);
+            rangeEnd = new Double(end);
+        }
+        range = rangeEnd - rangeStart;
+    }
+    
+    public void setRange(double start, double end) {
+        if (start > end) {
+            rangeStart = end;
+            rangeEnd = start;
+        } else {
+            rangeStart = start;
+            rangeEnd = end;
+        }
+        range = rangeEnd - rangeStart;
+    }
+
+    public int readValueAsInt() {
+        int res = reading.nextInt(rangeEnd.intValue()) + rangeStart.intValue();
+        return res;
+    }
+    
+    public double readValueAsDouble() {
+        double res = reading.nextDouble() * range + rangeStart;
+        return res;
+    }
+    
+    public boolean readStatus() {
+        int value = reading.nextInt();
+        boolean res = false;
+        
+        if (value == 0) res = true;
+        else res = false;
+        
+        return res;
+    }
+}
