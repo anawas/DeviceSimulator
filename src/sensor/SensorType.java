04ccecb3f33383f9b350812715d9887c17af3b3b
Andreas Wassmer <menkalinan56@gmail.com>
2017-01-30 17:31:17 +0100

    Moved all classes related to sensor to sensor package

---
+++ src/sensor/SensorType.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sensor;
+
+/**
+ *
+ * @author andreaswassmer
+ */
+public interface SensorType {
+    public final int TEMPERATURE = 0;
+    public final int HUMIDITY    = 1;
+    public final int BRIGHTNESS  = 2;
+    public final int AIRQUALITY  = 3;
+}
---
+++ src/sensor/TemperatureSensor.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sensor;
+
+/**
+ *
+ * @author andreaswassmer
+ */
+public class TemperatureSensor extends Sensor implements Measuring, SensorType {
+
+    public TemperatureSensor() {
+        super();
+        this.setRange(-10, 100);
+        type = SensorType.TEMPERATURE;
+        name = "Temperature";
+    }
+    
+    @Override
+    public String measure() {
+        int res = this.readValueAsInt();
+        return String.valueOf(res);
+    }  
+}
